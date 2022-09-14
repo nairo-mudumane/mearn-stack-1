43120cdfc9aa687498ad8ff664e0e4ca341faf80
nairo-mudumane <nairomudumane@outlook.com>
2022-09-14 14:33:25 +0200

    workouts model

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const workoutsRouter = require("./routes/works");
+const workoutsRouter = require("./routes/workouts");
 
 const app = express();
 const PORT = process.env.PORT;
