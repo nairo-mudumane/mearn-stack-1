e1348d492a91470d318a9346d2ca51ae4ac5784a
nairo-mudumane <nairomudumane@outlook.com>
2022-09-15 11:53:12 +0200

    mapping workouts

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,46 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import * as services from "../services";
 
 export function Home() {
+  const {
+    data: workouts,
+    isError,
+    isLoading,
+  } = useQuery(["/"], services.getAll, {
+    refetchInterval: 10000,
+    staleTime: 10000,
+  });
+
+  if (isLoading) {
+    return (
+      <div className="home">
+        <h3>Loading...</h3>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="home">
+        <h3>error fetching data</h3>
+      </div>
+    );
+  }
+
+  console.log(workouts);
+
   return (
     <div className="home">
       <h2>Home</h2>
+
+      {workouts && (
+        <div className="workouts">
+          {workouts.map((workout) => (
+            <p key={workout._id}>{workout.title}</p>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
