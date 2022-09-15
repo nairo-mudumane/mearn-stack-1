e46333c440c31431b6a368541e43682328d138b3
nairo-mudumane <nairomudumane@outlook.com>
2022-09-15 11:55:40 +0200

    cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const workoutsRouter = require("./routes/workouts");
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use("/workouts", workoutsRouter);
 
