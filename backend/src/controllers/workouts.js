d9bca341d5f998e40997c9de22de2441bebd3d52
nairo-mudumane <nairomudumane@outlook.com>
2022-09-16 10:25:10 +0200

    fixed error return

--- backend/src/controllers/workouts.js
+++ backend/src/controllers/workouts.js
@@ -27,7 +27,8 @@ const getAll = async (req, res) => {
 
     return res.status(200).json({ message: "ok", data: workouts });
   } catch (error) {
-    console.error;
+    console.error(error);
+    return res.status(500).json({ message: error.message });
   }
 };
 
